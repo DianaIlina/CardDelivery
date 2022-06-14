a1862b7386f6d6d08b69a81369ce41bb96cbe4a2
Диана Ильина <diana.1993.30@gmail.com>
2022-06-14 21:33:59 +0100

    Fixing homework

--- src/test/java/ru/netology/CardDeliveryServiceTest.java
+++ src/test/java/ru/netology/CardDeliveryServiceTest.java
@@ -1,34 +1,40 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryServiceTest {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     public void shouldCompleteDeliveryForm() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd-MM-yyyy", Locale.ENGLISH);
-        String deliveryDate = format1.format(ldt);
+        String deliveryDate = generateDate(3);
 
         Configuration.headless = true;
         open("http://localhost:9999");
 
         $x("//*/span[@data-test-id=\"city\"]//input").setValue("Москва");
+        $x("//*/span[@data-test-id=\"date\"]//input")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $x("//*/span[@data-test-id=\"date\"]//input").setValue(deliveryDate);
         $x("//*/span[@data-test-id=\"name\"]//input").setValue("Петров Иван");
         $x("//*/span[@data-test-id=\"phone\"]//input").setValue("+79198885634");
         $x("//*/label[@data-test-id=\"agreement\"]").click();
         $(withText("Забронировать")).click();
 
-        $(withText("Встреча успешно забронирована ")).should(visible, Duration.ofSeconds(15));
+        $x("//*/div[@class=\"notification__content\"]").should(visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + deliveryDate));
     }
 }
